94f6a9437fcf16176fc85fbcdadf82d4a9d41aed
Amit singh <amitsinghraj2007@gmail.com>
2026-09-01 17:09:40 +0530

    two files added to lab3

--- lab3/prg1.js
+++ lab3/prg1.js
@@ -3,9 +3,9 @@ import http from "http";
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-    res.write("hello client");
-    res.end();
+  res.write("hello client");
+  res.end();
 });
-server.listen(3000,() => {
-    console.log("server is running on port 3000...");
+server.listen(3000, () => {
+  console.log("server is running on port 3000...");
 });
--- lab3/prg2.js
+++ lab3/prg2.js
@@ -1,9 +1,9 @@
-import http from 'http'
+import http from "http";
 
-const server = http.createServer((req,res) => {
-    res.write("<h1>Hello Client</h1>");
-     res.write("<h2>Amit Singh</h2>");
-    res.end();
+const server = http.createServer((req, res) => {
+  res.write("<h1>Hello Client</h1>");
+  res.write("<h2>Amit Singh</h2>");
+  res.end();
 });
 
 server.listen(4444, () => console.log("server is running at 4444.... "));
