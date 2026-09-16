43cdebe666f92073b2bd12950078e1f55662f14a
Amit singh <amitsinghraj2007@gmail.com>
2026-09-16 12:04:25 +0530

    add post request with body

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,15 +1,42 @@
 import http from "http";
 
-const server = http.createServer((req, res) => {
-  console.log("server hits");
+const server = http.createServer((req,res)=>{
+    if(req.url === '/' && req.method === 'GET'){
+      
+        res.statusCode = 200;
+        res.end("Get Request");
 
-  if (req.url === "/") {
-    res.statusCode = 200;
-    res.end("sent with 200");
-  } else {
-    res.statusCode = 404;
-    res.end("request not found");
-  }
+    }
+    else if(req.url === '/' && req.method === "POST"){
+      //console.log("Request:",req)
+      let body=''
+      req.on('data',(chunk)=>{
+        body += chunk
+      });
+      req.on("end",()=>{
+        const product=JSON.parse(body);
+        console.log("received product:",product);
+          res.statusCode = 200;
+        res.end(JSON.stringify({msg:'Product added',product}));
+      });
+      
+    }
+    else if(req.url === '/' && req.method === "PUT"){
+        res.statusCode = 200;
+        res.end(' PUT Request');
+    }
+    else if(req.url === '/' && req.method === "DELETE"){
+        res.statusCode = 200;
+        res.end(' DELETE Request');
+    }
+    else if(req.url === '/' && req.method === "DELETE"){
+        res.statusCode = 200;
+        res.end('DELETE Request');
+    }
+    else{
+        res.statusCode = 404;
+        res.end('request Not Found');
+    }
+
 });
-
-server.listen(5000, () => console.log("prg6 is running at 5000"));
+server.listen(5000,() => console.log("prg6 is running"));
